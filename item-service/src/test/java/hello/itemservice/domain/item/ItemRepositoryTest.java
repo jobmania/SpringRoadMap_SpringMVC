8f363fc4b9512559a64d68065b648d828f80ce43
jobmania <xzxz7003@naver.com>
2023-04-03 02:44:49 +0900

    타임리프를 활용해 렌더링이 완료되면 model의 값을 주입하여 html로 보여주는 작업

--- item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,7 +1,12 @@
 package hello.itemservice.domain.item;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ItemRepositoryTest {
     ItemRepository itemRepository = new ItemRepository();
@@ -14,28 +19,52 @@ class ItemRepositoryTest {
     @Test
     void save(){
         //given
+        Item item = new Item("itemA", 10000, 10);
+
 
         //when
-
+        Item saveItem = itemRepository.save(item);
         //then
+        Item findItem = itemRepository.findById(item.getId());
+
+        assertThat(findItem).isEqualTo(saveItem);
+
     }
     @Test
     void findAll(){
         //given
-        Item item = new Item("itemA", 10000, 10)
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 10000, 10);
 
+        itemRepository.save(item1);
+        itemRepository.save(item2);
 
         //when
 
+        List<Item> result = itemRepository.findAll();
+
         //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+
     }
     @Test
     void update(){
         //given
-
+        Item item1 = new Item("itemA", 10000, 10);
+        Item saveItem = itemRepository.save(item1);
+        Long itemId = saveItem.getId();
         //when
 
+        Item updateParam = new Item("itemB22", 200000, 200);
+        itemRepository.update(itemId,updateParam);
+
         //then
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo("itemB22");
+        assertThat(findItem.getPrice()).isEqualTo(200000);
+        assertThat(findItem.getQuantity()).isEqualTo(200);
     }
 
 
