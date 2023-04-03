5b173aec5a3be2e1303470dfac899a7c6cba8c97
jobmania <xzxz7003@naver.com>
2023-04-04 02:33:35 +0900

    POST 요청후 Redirect를 하여 새로고침을하여도 동일한 요청이 되지 않게 + 저장이 될시 저장이 되었다는 메세지 출력

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -19,22 +19,109 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
 
+    /**
+     * Test 용 데이터 추가
+     */
+    @PostConstruct // 선주문
+    public void init() {
+        itemRepository.save(new Item("itemA", 12200, 10));
+        itemRepository.save(new Item("itemb", 92200, 130));
+    }
+
     @GetMapping
-    public String items(Model model){
+    public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
     }
 
-    /**
-     *  Test 용 데이터 추가
-     * */
-    @PostConstruct // 선주문
-    public void init(){
-        itemRepository.save(new Item("itemA", 12200, 10));
-        itemRepository.save(new Item("itemb", 92200, 130));
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(
+          @ModelAttribute("item") Item item) {
+// @ModelAttribute model을 만들어주고, model에 주입함 , Model model도 생략가능!
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 생략가능! 자동추가니까
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // 이름을 지정하지 않으면 Item(클래스명) -> item이 된다.
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
 
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        // @ModelAttribute 생략 가능
+        itemRepository.save(item);
 
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        // @ModelAttribute 생략 가능
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        // @ModelAttribute 생략 가능
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId());
+        redirectAttributes.addAttribute("status", true); // ?status=true
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}"; // 리다이렉트!!
+    }
 }
