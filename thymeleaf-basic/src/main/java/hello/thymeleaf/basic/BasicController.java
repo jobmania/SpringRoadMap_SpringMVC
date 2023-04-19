fe619759f5b77bb1360a2d61c11e5bdda0c2e6d8
jobmania <xzxz7003@naver.com>
2023-04-19 23:17:32 +0900

    <Spring-mvc2> : 리터럴 /  입력시 작은 따옴표 활용.                            연산 /

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -75,6 +75,22 @@ public class BasicController {
     }
 
 
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
