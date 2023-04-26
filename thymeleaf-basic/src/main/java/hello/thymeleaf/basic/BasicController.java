198d7e70e7233e2579ceec63fa4c63fd23cc29f3
Son won cheol <xzxz7003@naver.com>
2023-04-26 16:01:31 +0900

    <Spring-mvc2> : 블록 /
    여러 div 를 사용할때

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -117,7 +117,21 @@ public class BasicController {
     }
 
 
-    
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 23));
+
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+
 
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
