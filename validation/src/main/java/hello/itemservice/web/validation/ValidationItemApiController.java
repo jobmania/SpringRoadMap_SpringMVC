25d97c6c944eb3d3333e8b9519bc8060e5fb4205
Son won cheol <xzxz7003@naver.com>
2023-05-25 15:20:29 +0900

    <Spring-mvc2> : 회원 비즈니스 로직 추가
    domain : service, 객체 
    web:  controller 
    이렇게 저장하는 이유 , 다른 기술로 바꾸어도 도메인은 그대로 유지할 수 있어야 한다

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -1,2 +1,30 @@
-package hello.itemservice.web.validation;public class ValidationItemApiController {
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult){
+        log.info("api 컨트롤러 확인");
+        if(bindingResult.hasErrors()){
+            log.info("검증오류!! errors={} ", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+
+        log.info("성공로직실행");
+        return form;
+
+    }
 }
