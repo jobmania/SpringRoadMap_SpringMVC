1983d1f63e32f2f26367b569ceb303839217c39b
Son won cheol <xzxz7003@naver.com>
2023-05-26 16:29:25 +0900

    <Spring-mvc2> : 인터셉터를 활용, 로그 찍기

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -82,7 +83,7 @@ public class LoginController {
     }
 
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request){
 
         if(bindingResult.hasErrors()){
@@ -109,6 +110,38 @@ public class LoginController {
 
         return "redirect:/";
     }
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/", name = "redirectURL") String redirectURL,
+                          HttpServletRequest request){
+
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember == null){ /// 각각의 field의 error는 아니고 못찾은 거임ㅇㅇ
+            // 글로벌 error 메세지 설정!
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.!");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리 로직
+        // 세션이 있다면 세션반환, 없다면 신규 세션을 반환한다.
+        HttpSession session = request.getSession(); /// getSession(true) default: true
+        // 세션이 로그인 회원 정보를 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+
+        // 세션 관리자를 이용해서 세션  생성, 회원 데이터를 보관!!
+
+
+        return "redirect:"+redirectURL;
+    }
+
+
+
 
 //    @PostMapping("/logout")
     public String logout(HttpServletResponse response){
