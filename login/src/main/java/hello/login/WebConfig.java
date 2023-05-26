814c3697c5c10928c1ba2bc539fa5777f42e9aa9
Son won cheol <xzxz7003@naver.com>
2023-05-26 16:29:25 +0900

    <Spring-mvc2> :  필터를 이용한 로그인,비로그인 별 구분
    또한 Redirect를 통해 로그인 이전 화면으로 이동

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,2 +1,32 @@
-package hello.login;public class WebConfig {
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean doFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+    @Bean
+    public FilterRegistrationBean logCheckFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*"); /// 여기에 이전에 추가했음
+
+        return filterRegistrationBean;
+    }
 }
